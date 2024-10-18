187a716aec88fb99d748dae44f1dced9345eafbe
manuelguirado <85970925+manuelguirado@users.noreply.github.com>
2024-10-18 18:06:05 +0200

    Add files via upload

---
+++ teachphp/bucles.php
@@ -0,0 +1,43 @@
+<?php
+   //  bucle while
+   /*
+   $contador = 0;
+   while ($contador <= 10) {
+       echo  $contador . "<br>";
+       $contador++;
+   }
+   */
+   //bucle for
+   //>= <= -> !== == === !=
+   /*
+   for ($contador = 0; $contador <= 10; $contador++) {
+       echo $contador;
+   }
+       */
+   //bucle do while
+   /*
+   $contador = 0;
+   do {
+       echo $contador;
+       $contador++;
+   } while ($contador <= 10);
+    */
+   //bucle switch
+   /*
+   $age = 29;
+   switch ($age) {
+       case 18:
+           echo "eres mayor de edad";
+           break;
+       case 25:
+           echo "eres mayor de edad";
+           break;
+       case 40:
+           echo "eres mayor de edad";
+           break;
+       default:
+           echo "eres menor de edad";
+           break;
+   }
+           */
+?>
