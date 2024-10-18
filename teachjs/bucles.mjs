a578bbddb25856f5e102f8354654be1203f329ad
manuelguirado <85970925+manuelguirado@users.noreply.github.com>
2024-10-18 17:44:24 +0200

    bucles en js

---
+++ teachjs/bucles.mjs
@@ -0,0 +1,45 @@
+//bucle while
+/*
+let  contador = 0;
+while (contador <= 10) {
+    console.log(contador)
+    contador++ 
+
+}
+    */
+   //bucle for
+   //>= <= -> !== == === !=
+   /*
+   for (let contador = 0; contador <= 10; contador++) {
+    console.log(contador)
+   }
+   */
+  //bucle do while
+  /*
+  let  contador = 0;
+  do {
+    console.log(contador)
+    contador++
+
+  } while (contador <= 10);
+   */
+  //bucle switch
+  /*
+  let age  = 29;
+  switch (age) {
+      case (age >= 18):
+          console.log("joven");
+          break;
+      case (age >= 25):
+          console.log("adulto");
+          break;
+      case (age >= 30):
+          console.log("viejo");
+          break;
+      
+      default:
+          console.log("dame una edad pisha");
+          break;
+  }
+          */
+         
