45dd3b933fb3d5b2643799514882c0371a94cbad
manuelguirado <85970925+manuelguirado@users.noreply.github.com>
2024-10-18 17:59:39 +0200

    if en php

---
+++ teachphp/condicionales.php
@@ -0,0 +1,10 @@
+<?php
+    $age  = 31;
+    if ($age >= 18 && $age <= 25) {
+        echo "eres un adulto";
+    }else if ( $age > 25  && $age <= 40) {
+        echo "eres un viejo y no lo reconoces";
+    }else {
+        echo "ere viejo";
+    }
+?>
