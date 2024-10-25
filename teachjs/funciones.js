259cdabddf87d9d6d35199f140a3bbee401bceb6
manuelguirado <85970925+manuelguirado@users.noreply.github.com>
2024-10-25 16:16:50 +0200

    funciones en js

---
+++ teachjs/funciones.js
@@ -0,0 +1,16 @@
+//normal function
+function saludar() {
+    console.log("hola");
+}
+saludar();
+//arrow function
+let  saludar2 = () => {
+    console.log("hola");
+}
+saludar2();
+//function expression
+ 
+  let  saludar3 = function () {
+    console.log("hola");
+}
+saludar3();
